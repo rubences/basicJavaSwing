b4ede2a30072309d8b9cc0d57f074416eddf3f0a
Rubén Juárez <91731294+rubences@users.noreply.github.com>
2022-02-14 19:33:43 +0000

    Copias

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import javax.swing.SwingConstants;
 
 class Main {
   public static void main(String[] args) {
-    JFrame frame = new JFrame("Hello world");
+    JFrame frame = new JFrame("Os estoy enseñando a copiar bien");
     frame.setSize(300, 300);
     frame.setLocation(5, 5);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
